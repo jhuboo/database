f198a5257efa7cc1310d899d2687a2745aa0f4a1
Anvesh <rgjhuboo@gmail.com>
2022-01-31 17:34:28 -0500

    Add close_input_buffer(): frees the allocated memory for an instance of InputBuffer* and the buffer element of the respective structure

--- main.c
+++ main.c
@@ -35,6 +35,11 @@ void read_input(InputBuffer* input_buffer) {
 	input_buffer->buffer[bytes_read - 1] = 0;
 }
 
+void close_input_buffer(InputBuffer* input_buffer) {
+	free(input_buffer->buffer);
+	free(input_buffer);
+}
+
 int main(int argc, char* argv[]) {
 
 	InputBuffer* input_buffer = new_input_buffer();
