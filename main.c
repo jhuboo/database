eb70d19f8c3924810c4f0e2a8cc3af3d5f80a2d8
Anvesh <rgjhuboo@gmail.com>
2022-01-31 17:32:52 -0500

    Add read_input(), because buffer starts as null, so getline allocates enough memory to hold a line of input and makes a buffer point to it

--- main.c
+++ main.c
@@ -14,6 +14,27 @@ InputBuffer* new_input_buffer() {
 	return input_buffer;
 }
 
+void print_prompt() {
+	printf("db > ");
+}
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+void read_input(InputBuffer* input_buffer) {
+	ssize_t bytes_read = 
+		getline(&(input_buffer->buffer), 
+			&(input_buffer->buffer_length, 
+			stdin));
+
+	if (bytes_read <= 0) {
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
+
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
 int main(int argc, char* argv[]) {
 
 	InputBuffer* input_buffer = new_input_buffer();
