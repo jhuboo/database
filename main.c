6cea6c3c6c9c93abea974092cef070151fe4d953
Anvesh <rgjhuboo@gmail.com>
2022-01-31 17:49:25 -0500

    Fixed error in main.c, if statement for strcmp needed to be inside REPL

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ typedef struct {
 } InputBuffer;
 
 InputBuffer* new_input_buffer() {
-	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+	InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
 	input_buffer->buffer = NULL;
 	input_buffer->buffer_length = 0;
 	input_buffer->input_length = 0;
@@ -26,13 +26,16 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void read_input(InputBuffer* input_buffer) {
 	ssize_t bytes_read = 
-		getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+		getline(&(input_buffer->buffer), 
+			&(input_buffer->buffer_length), 
+			stdin);
 
 	if (bytes_read <= 0) {
 		printf("Error reading input\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// Ignore trailing newline
 	input_buffer->input_length = bytes_read - 1;
 	input_buffer->buffer[bytes_read - 1] = 0;
 }
@@ -48,12 +51,12 @@ int main(int argc, char* argv[]) {
 	while (true) {
 		print_prompt();
 		read_input(input_buffer);
-	}
 
-	if (strcmp(input_buffer->buffer, ".exit") == 0) {
-		close_input_buffer(input_buffer);
-		exit(EXIT_SUCCESS);
-	} else {
-		printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		if (strcmp(input_buffer->buffer, ".exit") == 0) {
+			close_input_buffer(input_buffer);
+			exit(EXIT_SUCCESS);
+		} else {
+			printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		}
 	}
 }
