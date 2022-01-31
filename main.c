dc57b1288ac631f6750c9752296ead2b77387018
Anvesh <rgjhuboo@gmail.com>
2022-01-31 18:48:20 -0500

    Add serialize_row() & deserialize_row(): to convert to and from the compact representation

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 
+/* Compact Definition of a serialized Row */
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
@@ -14,7 +15,6 @@ const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
 
 typedef enum {
 	META_COMMAND_SUCCESS,
@@ -96,6 +96,18 @@ void execute_statement(Statement* statement) {
 			printf("This is where we would do a select.\n");
 			break;	
 	}
+}
+
+void serialize_row(Row* source, void *destination) {
+	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+	memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+	memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+	memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+	memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+	memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 void print_prompt() {
