995e4c0754d47ab4a2fda4a9c52ce4560d945003
Anvesh <rgjhuboo@gmail.com>
2022-01-31 18:16:21 -0500

    Add prepare_statement(): This is our SQL compiler; it only knows 2 commands right now - SELECT & INSERT

--- main.c
+++ main.c
@@ -46,6 +46,19 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 	}
 }
 
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
 void print_prompt() {
 	printf("db > ");
 }
