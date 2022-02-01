5ea5e6bc4f9a6be763f5ec5c8732d1670431856a
Anvesh <rgjhuboo@gmail.com>
2022-02-01 18:09:52 -0500

    Add get-node_type() & set_node_type()

--- main.c
+++ main.c
@@ -357,6 +357,16 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
 	serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
+NodeType get_node_type(void* node) {
+	uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+	return (NodeType)value;
+}
+
+void set_node_type(void* node, NodeType type) {
+	uint8_t value = type;
+	*((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
 Cursor* table_start(Table* table) {
 	Cursor* cursor = malloc(sizeof(Cursor));
 	cursor->table = table;
@@ -371,7 +381,7 @@ Cursor* table_start(Table* table) {
 }
 
 Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
-	void* node = get_pgae(table->pager, page_num);
+	void* node = get_page(table->pager, page_num);
 	uint32_t num_cells = *leaf_node_num_cells(node);
 
 	Cursor* cursor = malloc(sizeof(Cursor));
