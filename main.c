570466284d8f6acf7dbade0de05292e9d4fbe521
Anvesh <rgjhuboo@gmail.com>
2022-01-31 23:52:09 -0500

    Add struct for Pager, and refactor struct of Table to account for it

--- main.c
+++ main.c
@@ -63,8 +63,14 @@ const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
+	int file_descriptor;
+	uint32_t file_length;
+	void* pages[TABLE_MAX_PAGES];	
+} Pager;
+
+typedef struct {
+	Pager* pager;
 	uint32_t num_rows;
-	void* pages[TABLE_MAX_PAGES];
 } Table;
 
 
