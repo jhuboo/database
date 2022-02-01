50c23546e3ec996caff9723c556d37bb7f01df87
Anvesh <rgjhuboo@gmail.com>
2022-02-01 18:05:31 -0500

    Add leaf_node_find(): returns posiiton of the key, and performs Binary Search

--- main.c
+++ main.c
@@ -370,6 +370,35 @@ Cursor* table_start(Table* table) {
 	return cursor;
 }
 
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+	void* node = get_pgae(table->pager, page_num);
+	uint32_t num_cells = *leaf_node_num_cells(node);
+
+	Cursor* cursor = malloc(sizeof(Cursor));
+	cursor->table = table;
+	cursor->page_num = page_num;
+
+	// Binary Search
+	uint32_t min_index = 0;
+	uint32_t one_past_max_index = num_cells;
+	while (one_past_max_index != min_index) {
+		uint32_t index = (min_index + one_past_max_index) / 2;
+		uint32_t key_at_index = *leaf_node_key(node, index);
+		if (key == key_at_index) {
+			cursor->cell_num = index;
+			return cursor;
+		}
+		if (key < key_at_index) {
+			one_past_max_index = index;
+		} else {
+			min_index = index + 1;
+		}
+	}
+
+	cursor->cell_num = min_index;
+	return cursor;
+}
+
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void read_input(InputBuffer* input_buffer) {
