03e9a32c86339890287c80f8c7a5636e4fe6f1a7
Anvesh <rgjhuboo@gmail.com>
2022-02-01 16:42:39 -0500

    Add helper methods for accessing Leaf Node Fields

--- main.c
+++ main.c
@@ -126,6 +126,26 @@ const uint32_t LEAF_NODE_MAX_CELLS =
 		LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 
+uint32_t* leaf_node_num_cells(void* node) {
+	return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void* leaf_node_cell(void* node, uint32_t cell_num) {
+	return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
+	return leaf_node_cell(node, cell_num);
+}
+
+void* leaf_node_value(void* node, uint32_t cell_num) {
+	return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node) {
+	*leaf_node_num_cells(node) = 0;
+}
+
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
 	strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
