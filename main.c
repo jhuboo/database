806cff495610309522297a34dfe75ccbc8bee3d1
Anvesh <rgjhuboo@gmail.com>
2022-02-01 00:19:54 -0500

    Add get_page(), which has logic for handing a cache miss. We assume that pages are saved one after the other in the databse file - page 0 at offset 0, page 1 at offset 4096, page 2 at 8192, ... The page will be added to the file when we flush the cache to disk later

--- main.c
+++ main.c
@@ -94,6 +94,38 @@ void print_row(Row* row) {
 	printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
+void* get_page(Pager* pager, uint32_t page_num) {
+	if (page_num > TABLE_MAX_PAGES) {
+		printf("Tried to fetch page number out of bounds. %d > %d\n",
+			page_num, TABLE_MAX_PAGES);
+		exit(EXIT_FAILURE);
+	}
+
+	if (pager->pages[page_num] == NULL) {
+		// Cache misss. Allocate memory and load from file
+		void* page = malloc(PAGE_SIZE);
+		uint32_t num_pages = pager->file_length / PaGE_SIZE;
+		
+		// We might save a partial page at th eend of the file
+		if (pager->file_length % PAGE_SIZE) {
+			num_pages += 1;
+		}
+
+		if (page_num <= num_pages) {
+			lseek(page->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+			ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+			if (bytes_read == -1) {
+				printf("Error reading file: %d\n", errno);
+				exit(EXIT_FAILURE);
+			}
+		}	
+
+		pager->pages[page_num] = page;
+	}
+
+	return pager->pages[page_num];
+}
+
 void* row_slot(Table* table, uint32_t row_num) {
 	uint32_t page_num = row_num / ROWS_PER_PAGE;
 	void* page = get_page(table->pager, page_num);
