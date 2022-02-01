fcd95044d88ee0e33800ed80a82fd40dd28e510d
Anvesh <rgjhuboo@gmail.com>
2022-02-01 17:55:55 -0500

    Remove table_end() - we don't need it anymore

--- main.c
+++ main.c
@@ -370,19 +370,6 @@ Cursor* table_start(Table* table) {
 	return cursor;
 }
 
-Cursor* table_end(Table* table) {
-	Cursor* cursor = malloc(sizeof(Cursor));
-	cursor->table = table;
-	cursor->page_num = table->root_page_num;
-
-	void* root_node = get_page(table->pager, table->root_page_num);
-	uint32_t num_cells = *leaf_node_num_cells(root_node);
-	cursor->cell_num = num_cells;
-	cursor->end_of_table = true;
-
-	return cursor;
-}
-
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void read_input(InputBuffer* input_buffer) {
