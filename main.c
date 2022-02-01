1a5ed9feeb53d5ce1e5297372d78f8f438b7ead1
Anvesh <rgjhuboo@gmail.com>
2022-02-01 00:41:17 -0500

    Add pager_flush() - in the current design, the length of the file encodes how many rows are in the database, so we need to write a partial page at the end of the file - hence why pager_flush() takes both a page number and a size. Need to refactor this after implementing the B-tree

--- main.c
+++ main.c
@@ -160,6 +160,28 @@ Pager* pager_open(const char* filename) {
 	return pager;
 }
 
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+	if (pager->pages[page_num] == NULL) {
+		printf("Tried to flush null page\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+	if (offset == -1) {
+		printf("Error seeking: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	ssize_t bytes_written =
+		write(pager->file_descriptor, pager->pages[page_num], size);
+
+	if (bytes_written == -1) {
+		printf("Error writing: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+}
+
 Table* db_open(const char* filename) {
 	Pager* pager = pager_open(filename);
 	uint32_t num_rows = pager->file_length / ROW_SIZE;
